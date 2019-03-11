e820549dd44f5b316814489772940732a4cdf2de
pashakiz <pashakiz@gmail.com>
2019-03-11 03:30:18 +0300

    save, privacy done

--- js/common.js
+++ js/common.js
@@ -1,18 +1,7 @@
 $(document).ready(function() {
 
-    $('.header').on('click', '.header__btn', function(){
-        var header = $(this).closest('.header');
-        var btn = $(this).closest('.header').find('.header__btn');
-        var menu = $(this).closest('.header').find('.header__menu');
-        if ( btn.hasClass('icon-menu-down') ) {
-            menu.slideUp();
-            btn.removeClass('icon-menu-down');
-            btn.addClass('icon-menu');
-        } else {
-            menu.slideDown();
-            btn.removeClass('icon-menu');
-            btn.addClass('icon-menu-down');
-        }
+    $('.header-wrap').on('click', '.header__btn', function(){
+        $(this).closest('.header-wrap').find('.header-menu').slideToggle();
     });
     $('.custom-file-input').on('change', function(){
         //$('.custom-file-img').css('background-image', 'url(' + $(this).val() + ')');
